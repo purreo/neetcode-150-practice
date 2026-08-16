255299f3e3fc40340068ecbf5f6ab1c54322795b
Jesse <58563075+purreo@users.noreply.github.com>
2026-08-16 19:04:09 -0400

    Time: 279 ms (5.11%), Space: 25.9 MB (64.06%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,16 +1,20 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        # window size = 2
-        # longest substring with 2 unique characters
+        # only 2 baskets
+        # basket can hold only 1 fruit type
+        # pick 1 fruit from every tree 
+        # return max num of fruits that can fit into 2 baskets
+
+        # find longest substring with only 2 chars
+
+        win = Counter()
         l = 0
-        win = defaultdict(int)
-        longest = 0
-        for r in range(len(fruits)):
-            win[fruits[r]] += 1
+        ans = 0
+        for r,ch in enumerate(fruits):
+            win[ch] = win.get(ch,0) + 1
             while len(win) > 2:
                 win[fruits[l]] -= 1
                 if win[fruits[l]] == 0: del win[fruits[l]]
                 l += 1
-            longest = max(longest, r-l+1)
-        return longest
-
+            ans = max(ans,r-l+1)
+        return ans
